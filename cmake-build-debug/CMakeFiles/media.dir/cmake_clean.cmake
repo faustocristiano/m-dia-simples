ca490ec3db5a749f46adcd5374d75f2e9435b691
Fausto Cristiano <fausto.c@aluno.ifsc.edu.br>
2021-11-21 18:25:33 -0300

    concluído

---
+++ cmake-build-debug/CMakeFiles/media.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/media.dir/main.c.o"
+  "media"
+  "media.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/media.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  float x,y;
+  float media;
+
+  printf("Entre com x\n");
+  scanf("%f", &x);
+  printf("Entre com y\n");
+  scanf("%f", &y);
+  media = (x+y)/2;
+  printf("Valor de media = %f\n",media);
+  return 0;
+  }
